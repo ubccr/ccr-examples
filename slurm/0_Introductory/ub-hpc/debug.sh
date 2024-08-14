5cae3e7768f3a58c43cbeb5f15b0dbf7d453eeec
Dori Sajdak <djm29@buffalo.edu>
2024-08-14 10:42:09 -0400

    simple examples

---
+++ slurm/0_Introductory/ub-hpc/debug.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+
+#SBATCH --clusters=ub-hpc
+#SBATCH --partition=debug
+#SBATCH --qos=debug
+#SBATCH --time=01:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=64000
+
+
+
+
---
+++ slurm/0_Introductory/ub-hpc/general-compute.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+
+#SBATCH --clusters=ub-hpc
+#SBATCH --partition=general-compute
+#SBATCH --qos=general-compute
+#SBATCH --time=12:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=64000
+
+
+
+
